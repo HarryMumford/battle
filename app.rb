c3fa3860c24edfbefb37ae5fd914cfb33de72878
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-11-25 17:41:51 +0000

    add erb

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "hello"
 end
 
 get '/secret' do
-  "tuna"
+  "big ol' tuna"
+end
+
+get '/cat' do
+  erb(:index)
+  # erb "hi tuna <%= 68 + 1 %>!"
 end
